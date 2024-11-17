5c55c7181372d942e72d31a25d264ed30a9ad29f
Stefan Gabos <contact@stefangabos.ro>
2024-11-17 18:06:06 +0200

    Minor source code update

--- gruntfile.js
+++ gruntfile.js
@@ -1,78 +1,74 @@
 'use strict';
+
 const sass = require('sass');
 
 module.exports = grunt => {
-	grunt.initConfig({
-		sass: {
-			options: {
-				implementation: sass
-			},
-			compile: {
-				files: {
-					'test/tmp/compile.css': 'test/fixtures/test.scss',
-					'test/tmp/compile2.css': 'test/fixtures/test.scss'
-				}
-			},
-			modernCompile: {
-				options: {
-					api: 'modern'
-				},
-				files: {
-					'test/tmp/modern-compile.css': 'test/fixtures/test.scss',
-					'test/tmp/modern-compile2.css': 'test/fixtures/test.scss'
-				}
-			},
-			includePaths: {
-				options: {
-					includePaths: ['test/fixtures']
-				},
-				files: {
-					'test/tmp/include-paths.css': 'test/fixtures/include-paths.scss'
-				}
-			},
-			ignorePartials: {
-				cwd: 'test/fixtures/partials',
-				src: '*.scss',
-				dest: 'test/tmp',
-				expand: true,
-				ext: '.css'
-			},
-			sourceMap: {
-				options: {
-					sourceMap: 'test/tmp/source-map.css.map'
-				},
-				files: {
-					'test/tmp/source-map.css': 'test/fixtures/test.scss'
-				}
-			},
-			sourceMapSimple: {
-				options: {
-					sourceMap: true
-				},
-				files: {
-					'test/tmp/source-map-simple.css': 'test/fixtures/test.scss'
-				}
-			},
-			precision: {
-				options: {
-					precision: 3
-				},
-				files: {
-					'test/tmp/precision.css': 'test/fixtures/precision.scss'
-				}
-			}
-		},
-		nodeunit: {
-			tasks: ['test/test.js']
-		},
-		clean: {
-			test: ['test/tmp/**']
-		}
-	});
 
-	grunt.loadTasks('tasks');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.initConfig({
 
-	grunt.registerTask('default', ['clean', 'sass', 'nodeunit', 'clean']);
+        sass: {
+            options: {
+                implementation: sass,
+                indentWidth: 4,
+                api: 'modern'
+            },
+            compile: {
+                files: {
+                    'test/tmp/compile.css': 'test/fixtures/test.scss',
+                    'test/tmp/compile2.css': 'test/fixtures/test.scss'
+                }
+            },
+            includePaths: {
+                options: {
+                    includePaths: ['test/fixtures']
+                },
+                files: {
+                    'test/tmp/include-paths.css': 'test/fixtures/include-paths.scss'
+                }
+            },
+            ignorePartials: {
+                cwd: 'test/fixtures/partials',
+                src: '*.scss',
+                dest: 'test/tmp',
+                expand: true,
+                ext: '.css'
+            },
+            sourceMap: {
+                options: {
+                    sourceMap: 'test/tmp/source-map.css.map'
+                },
+                files: {
+                    'test/tmp/source-map.css': 'test/fixtures/test.scss'
+                }
+            },
+            sourceMapSimple: {
+                options: {
+                    sourceMap: true
+                },
+                files: {
+                    'test/tmp/source-map-simple.css': 'test/fixtures/test.scss'
+                }
+            },
+            precision: {
+                options: {
+                    precision: 3
+                },
+                files: {
+                    'test/tmp/precision.css': 'test/fixtures/precision.scss'
+                }
+            }
+        },
+        nodeunit: {
+            tasks: ['test/test.js']
+        },
+        clean: {
+            test: ['test/tmp/**']
+        }
+    });
+
+    grunt.loadTasks('tasks');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+    grunt.registerTask('default', ['clean', 'sass', 'nodeunit', 'clean']);
 };
