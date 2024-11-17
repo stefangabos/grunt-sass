1ac0025e2ff3c932d9c6052d2aa5728bfa815650
Stefan Gabos <contact@stefangabos.ro>
2024-11-17 18:05:44 +0200

    Minor source code formatting update

--- test/test.js
+++ test/test.js
@@ -1,71 +1,68 @@
 'use strict';
+
 const grunt = require('grunt');
 
 exports.sass = {
-	compile(test) {
-		test.expect(2);
 
-		const actual = grunt.file.read('test/tmp/compile.css');
-		const actual2 = grunt.file.read('test/tmp/compile2.css');
-		const expected = grunt.file.read('test/expected/compile.css');
-		test.equal(actual, expected, 'should compile SCSS to CSS');
-		test.equal(actual2, expected, 'should compile SCSS to CSS');
+    compile(test) {
+        test.expect(2);
 
-		test.done();
-	},
-	modernCompile(test) {
-		test.expect(2);
+        const actual = grunt.file.read('test/tmp/compile.css');
+        const actual2 = grunt.file.read('test/tmp/compile2.css');
+        const expected = grunt.file.read('test/expected/compile.css');
+        test.equal(actual, expected, 'should compile SCSS to CSS');
+        test.equal(actual2, expected, 'should compile SCSS to CSS');
 
-		const actual = grunt.file.read('test/tmp/modern-compile.css');
-		const actual2 = grunt.file.read('test/tmp/modern-compile2.css');
-		const expected = grunt.file.read('test/expected/compile.css');
-		test.equal(actual, expected, 'should compile SCSS to CSS');
-		test.equal(actual2, expected, 'should compile SCSS to CSS');
+        test.done();
+    },
 
-		test.done();
-	},
-	includePaths(test) {
-		test.expect(1);
+    includePaths(test) {
+        test.expect(1);
 
-		const actual = grunt.file.read('test/tmp/include-paths.css');
-		const expected = grunt.file.read('test/expected/include-paths.css');
-		test.equal(actual, expected, 'should compile SCSS to CSS with options');
+        const actual = grunt.file.read('test/tmp/include-paths.css');
+        const expected = grunt.file.read('test/expected/include-paths.css');
+        test.equal(actual, expected, 'should compile SCSS to CSS with options');
 
-		test.done();
-	},
-	ignorePartials(test) {
-		test.expect(1);
+        test.done();
+    },
 
-		test.ok(!grunt.file.exists('test/tmp/_partial.css'), 'underscore partial files should be ignored');
+    ignorePartials(test) {
+        test.expect(1);
 
-		test.done();
-	},
-	sourceMap(test) {
-		test.expect(2);
+        test.ok(!grunt.file.exists('test/tmp/_partial.css'), 'underscore partial files should be ignored');
 
-		const css = grunt.file.read('test/tmp/source-map.css');
-		test.ok(/\/\*# sourceMappingURL=source-map\.css\.map/.test(css), 'should include sourceMapppingUrl');
+        test.done();
+    },
 
-		const map = grunt.file.read('test/tmp/source-map.css.map');
-		test.ok(/test\.scss/.test(map), 'should include the main file in sourceMap at least');
-		test.done();
-	},
-	sourceMapSimple(test) {
-		test.expect(2);
+    sourceMap(test) {
+        test.expect(2);
 
-		const css = grunt.file.read('test/tmp/source-map-simple.css');
-		test.ok(/\/\*# sourceMappingURL=source-map-simple\.css\.map/.test(css), 'should include sourceMappingUrl');
+        const css = grunt.file.read('test/tmp/source-map.css');
+        test.ok(/\/\*# sourceMappingURL=source-map\.css\.map/.test(css), 'should include sourceMapppingUrl');
 
-		const map = grunt.file.read('test/tmp/source-map-simple.css.map');
-		test.ok(/test\.scss"/.test(map), 'should include the main file in sourceMap at least');
-		test.done();
-	},
-	precision(test) {
-		test.expect(1);
+        const map = grunt.file.read('test/tmp/source-map.css.map');
+        test.ok(/test\.scss/.test(map), 'should include the main file in sourceMap at least');
+        test.done();
+    },
 
-		const actual = grunt.file.read('test/tmp/precision.css');
-		test.ok(/1\.343/.test(actual), 'should support precision option');
+    sourceMapSimple(test) {
+        test.expect(2);
 
-		test.done();
-	}
+        const css = grunt.file.read('test/tmp/source-map-simple.css');
+        test.ok(/\/\*# sourceMappingURL=source-map-simple\.css\.map/.test(css), 'should include sourceMappingUrl');
+
+        const map = grunt.file.read('test/tmp/source-map-simple.css.map');
+        test.ok(/test\.scss"/.test(map), 'should include the main file in sourceMap at least');
+        test.done();
+    },
+
+    precision(test) {
+        test.expect(1);
+
+        const actual = grunt.file.read('test/tmp/precision.css');
+        test.ok(/1\.343/.test(actual), 'should support precision option');
+
+        test.done();
+    }
+
 };
