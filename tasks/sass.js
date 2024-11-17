8d5cfec5822d8c81dc2e83d225891ee579feda0c
Stefan Gabos <contact@stefangabos.ro>
2024-11-17 18:06:46 +0200

    Fixed broken source map generation

--- tasks/sass.js
+++ tasks/sass.js
@@ -4,45 +4,52 @@ const util = require('util');
 const path = require('path');
 
 module.exports = grunt => {
-	grunt.registerMultiTask('sass', 'Compile Sass to CSS', function () {
-		const done = this.async();
+    grunt.registerMultiTask('sass', 'Compile Sass to CSS', function () {
 
-		const options = this.options({
-			precision: 10
-		});
+        const done = this.async();
+        const options = this.options({
+            precision: 10
+        });
 
-		if (!options.implementation) {
-			grunt.fatal('The implementation option must be passed to the Sass task');
-		}
-		grunt.verbose.writeln(`\n${options.implementation.info}\n`);
+        let filePath = '';
 
-		(async () => {
-			await Promise.all(this.files.map(async item => {
-				const [src] = item.src;
-				let result;
+        if (!options.implementation) {
+            grunt.fatal('The implementation option must be passed to the Sass task');
+        }
+        grunt.verbose.writeln(`\n${options.implementation.info}\n`);
 
-				if (!src || path.basename(src)[0] === '_') {
-					return;
-				}
+        (async () => {
+            await Promise.all(this.files.map(async item => {
+                const [src] = item.src;
+                let result;
 
-				if (options.api === 'modern') {
-					result = await options.implementation.compileAsync(src, options);
-				} else {
-					result = await util.promisify(options.implementation.render)(Object.assign({}, options, {
-						file: src,
-						outFile: item.dest
-					}));
-				}
+                if (!src || path.basename(src)[0] === '_') {
+                    return;
+                }
 
-				grunt.file.write(item.dest, result.css);
+                if (options.api === 'modern') {
+                    result = await options.implementation.compileAsync(src, options);
+                } else {
+                    result = await util.promisify(options.implementation.render)(Object.assign({}, options, {
+                        file: src,
+                        outFile: item.dest
+                    }));
+                }
 
-				if (options.sourceMap) {
-					const filePath = options.sourceMap === true ? `${item.dest}.map` : options.sourceMap;
-					grunt.file.write(filePath, result.map);
-				}
-			}));
-		})().catch(error => {
-			grunt.fatal(error.formatted || error);
-		}).then(done);
-	});
+                if (options.sourceMap) {
+                    filePath = options.sourceMap === true ? `${item.dest}.map` : options.sourceMap;
+                    grunt.file.write(filePath, JSON.stringify(result.sourceMap));
+                }
+
+                if (options.sourceMap) {
+                    result.css = '/*# sourceMappingURL=' + path.basename(filePath) + '*/' + "\n" + result.css;
+                }
+
+                grunt.file.write(item.dest, result.css);
+
+            }));
+        })().catch(error => {
+            grunt.fatal(error.formatted || error);
+        }).then(done);
+    });
 };
